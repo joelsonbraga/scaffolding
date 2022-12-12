75b87bd2cff5f4be0d7c488c131fee31f585e160
Joelson Braga <joelsonbraga@gmail.com>
2022-12-12 15:58:08 -0300

    Teste de conexão via PDO

--- test.php
+++ test.php
@@ -2,6 +2,8 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
+require_once 'lib/Database/ConnectionDatabase.php';
+require_once 'lib/Database/ResponseData.php';
 require_once 'Services/NaturezaOperacaoService.php';
 
 $natureza = new \Services\NaturezaOperacaoService();
